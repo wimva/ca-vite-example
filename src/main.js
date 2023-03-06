a3dba9f9c7fcc0d1db2fa2acb02573b1bc4e0145
Wim Van Aerschot <wim@vaw.be>
2023-03-06 15:20:21 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import './styles/reset.css';
+import './styles/style.css';
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
